c13eb0a86c5c1b760656f7aa2ccc330bfc51546a
Tushar-0202-Shaw <tusharshaw987@gmail.com>
2024-06-28 16:26:43 +0530

    Find Nth node from the end of linked list

---
+++ Question_2.java
@@ -0,0 +1,38 @@
+package Singly_Linked_Interview_Questions;
+public class Question_2 {
+    public static node nthNode(node head, int n) {
+        int size = 0;
+        node temp = head;
+        while (temp != null) {
+            size++;
+            temp = temp.next;
+        }
+        int m = size-n+1;
+        temp = head;
+        for (int i=1; i<=m-1 ; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public static void display(node head) {
+        if (head == null) return;
+        System.out.print(head.data + " ");
+        display(head.next);
+    }
+
+    public static void main(String[] args) {
+        node a = new node(1);
+        node b = new node(2);
+        node c = new node(3);
+        node d = new node(4);
+        node e = new node(5);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        display(a);
+        System.out.println();
+        System.out.println(nthNode(a, 3).data);
+    }
+}
